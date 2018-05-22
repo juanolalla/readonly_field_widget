7fa09a0ecf740d8cc1d94846133d35c88fcb074f
Juan Olalla <juan.olalla@gmail.com>
2018-05-22 19:08:19 +0200

    Move field types filtering logic to static function in the widget class

--- src/Plugin/Field/FieldWidget/ReadonlyWidget.php
+++ src/Plugin/Field/FieldWidget/ReadonlyWidget.php
@@ -24,4 +24,23 @@ class ReadonlyWidget extends WidgetBase implements WidgetInterface {
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     return $items->view();
   }
+
+  /**
+   * Gets all field types that can use the Readonly widget.
+   *
+   * In ReadonlyWidget::formElement() the field is rendered using its default
+   * formatter as the form element array. This would not work for a field type
+   * that does not provide a default formatter, resulting in a PHP error. For
+   * that reason we are filtering the field types allowed for this widget.
+   *
+   * @see readonly_field_widget.module
+   * @see Implements hook_field_widget_info_alter()
+   */
+  public static function fieldTypes() {
+    $field_types = \Drupal::service('plugin.manager.field.field_type')->getDefinitions();
+    $field_types = array_filter($field_types, function ($item) {
+      return isset($item['default_formatter']);
+    });
+    return array_keys($field_types);
+  }
 }
